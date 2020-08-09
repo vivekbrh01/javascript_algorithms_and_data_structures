b11fd0a4f236ed09d3edfdfe15d1ceb88bee4cb0
Vivek Baruah <vivekbrh01@gmail.com>
2020-08-09 22:10:25 +0530

    Pop, shift and unshift implemented in Doubly Linked List

--- doubly_linked_list.js
+++ doubly_linked_list.js
@@ -27,6 +27,52 @@ class DoublyLinkedList {
 		this.length++;
 		return this;
 	}
+
+	pop() {
+		if (!this.head) return undefined;
+		let temp = this.tail;
+		if (this.length === 1) {
+			this.head = null;
+			this.tail = null;
+		} else {
+			this.tail = temp.prev;
+			this.tail.next = null;
+			temp.prev = null;
+		}
+
+		this.length--;
+		return this;
+	}
+
+	unshift(data) {
+		let node = new Node(data);
+		if (!this.head) {
+			this.head = node;
+			this.tail = node;
+		} else {
+			let temp = this.head;
+			this.head = node;
+			node.next = temp;
+			temp.prev = node;
+		}
+		this.length++;
+		return this;
+	}
+
+	shift() {
+		if (!this.head) return undefined;
+		let temp = this.head;
+		if (this.length === 1) {
+			this.head = null;
+			this.tail = null;
+		} else {
+			this.head = temp.next;
+			this.head.prev = null;
+			temp.next = null;
+		}
+		this.length--;
+		return this
+	}
 }
 
 let list = new DoublyLinkedList();
@@ -34,4 +80,7 @@ let list = new DoublyLinkedList();
 list.push(1);
 list.push(2);
 list.push(3);
+// list.pop();
+// list.unshift(0);
+list.shift()
 console.log(list);
