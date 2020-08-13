6b0875a0ac37e386470157c36c0f674afc43bc6e
Vivek Baruah <vivekbrh01@gmail.com>
2020-08-13 23:56:20 +0530

    Priority Queue

--- queue.js
+++ queue.js
@@ -1,6 +1,7 @@
+// Queue
 function Queue() {
-    collection = [];
-    
+	collection = [];
+
 	this.print = function () {
 		console.log(collection);
 	};
@@ -25,3 +26,65 @@ function Queue() {
 		return collection.length === 0;
 	};
 }
+
+let q = new Queue();
+q.enqueue("a");
+q.enqueue("b");
+q.enqueue("c");
+q.print();
+q.dequeue();
+console.log(q.front());
+q.print();
+
+// Priority Queue
+function PriorityQueue() {
+	let collection = [];
+	this.printCollection = function () {
+		console.log(collection);
+	};
+
+	this.enqueue = function (element) {
+		if (this.isEmpty()) {
+			collection.push(element);
+		} else {
+			let added = false;
+			for (let i = 0; i < collection.length; i++) {
+				if (element[1] < collection[i][1]) {
+					collection.splice(i, 0, element);
+					added = true;
+					break;
+				}
+			}
+
+			if (!added) {
+				collection.push(element);
+			}
+		}
+	};
+
+	this.dequeue = function () {
+		let value = collection.shift();
+		return value[0];
+	};
+
+	this.front = function () {
+		return collection[0];
+	};
+
+	this.size = function () {
+		return collection.length;
+	};
+
+	this.isEmpty = function () {
+		return collection.length === 0;
+	};
+}
+
+let pq = new PriorityQueue();
+pq.enqueue(["Chao Vivek", 2]);
+pq.enqueue(["Changmai Baruah", 3]);
+pq.enqueue(["Z Dr", 1]);
+pq.printCollection();
+pq.dequeue();
+pq.front();
+pq.printCollection();
