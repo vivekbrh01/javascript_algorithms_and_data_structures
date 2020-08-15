9cbbfe40b4af7350f2e06ea53ca4ca1ef7d462dc
Vivek Baruah <vivekbrh01@gmail.com>
2020-08-15 23:52:35 +0530

    Selection sort

---
+++ sorting.js
@@ -0,0 +1,16 @@
+// Selection Sort
+function selectionSort(arr) {
+	const array = arr.slice();
+	for (let i = 0; i < array.length - 1; i++) {
+		let minIndex = i;
+		for (let j = i + 1; j < array.length; j++) {
+			if (array[j] < array[minIndex]) {
+				minIndex = j;
+			}
+		}
+		const temp = array[i];
+		array[i] = array[minIndex];
+		array[minIndex] = temp;
+	}
+	return array;
+}
