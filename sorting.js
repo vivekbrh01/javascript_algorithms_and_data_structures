7f5cb7b4958b362aad11bd574a5734ecdfd55ae0
Vivek Baruah <vivekbrh01@gmail.com>
2020-08-16 00:00:04 +0530

    Bubble sort

--- sorting.js
+++ sorting.js
@@ -14,3 +14,21 @@ function selectionSort(arr) {
 	}
 	return array;
 }
+
+const array = [3, 4, 5, 1, 8, 7, 11, 9];
+const output = selectionSort(array);
+console.log(output);
+console.log(array);
+
+// Bubble sort
+function bubbleSort(array) {
+	for (let i = 0; i < array.length - 1; i++) {
+		for (let j = 0; j < array.length - 1 - i; j++) {
+			if (array[j] > array[j + 1]) {
+				const temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
+		}
+	}
+}
